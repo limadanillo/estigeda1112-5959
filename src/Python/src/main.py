fa7c2a3af4e17524862b33f954ae804fca5e2ec2
miguel.c.rodrigues <miguel.c.rodrigues@gmail.com>
2012-06-03 15:30:55 +0000

    Python source code project created: Simple "HelloWorld" Test

---
+++ src/Python/src/main.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+'''
+Escola Superior de Tecnologias e Gestão de Beja - ESTIG
+Licenciatura em Engenharia Informática - 9119
+Estrura de Dados e Algoritmos - EDA
+-----------------------------------
+Created on June 13th, 2012
+@author: Miguel de Campos Rodrigues - 5959
+'''
+
+def main():
+    print "HelloWorld"
+    pass
+
+if __name__ == '__main__':
+    main()
+    pass
