1a24195fc5e9fcf73688e4a21c7d22592dda977d
miguel.c.rodrigues <miguel.c.rodrigues@gmail.com>
2012-06-15 18:30:48 +0000

    Added Python/src/estigeda1112-5959/kDTree/Knot.py Added Python/src/estigeda1112-5959/kDTree/Tree.py
    Created "Knot" class

---
+++ src/Python/src/estigeda1112-5959/kDTree/Tree.py
@@ -0,0 +1,18 @@
+'''
+Created on 4 de Jun de 2012
+
+@author: Miguel
+'''
+
+class Tree(object):
+    '''
+    classdocs
+    '''
+
+    def __init__():
+        '''
+        Constructor
+        '''
+        pass
+    
+    pass #class
